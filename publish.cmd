13456ee5194ee0fdb39431e7f5b96c17d9ad8617
Srikar Ravi <51483495+srikarsahara@users.noreply.github.com>
2022-02-17 22:21:31 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\demoqna9-bot-a7d6-Web-Deploy.pubxml -p:Password=vwEw0f6S3xuaGJWhtpNcDtv3KzRhreJBseS5CG4KPzbmiMkFHZcMqqihqHw7
+
